49710ff191753064bb6d28c26e8a082916918e0c
Chris Weicht <chris.weicht@gmail.com>
2018-11-26 06:10:46 -0500

    added ListView.builder

--- lib/products.dart
+++ lib/products.dart
@@ -3,25 +3,35 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   //final only means we won't "change" products but we can
   // replace it and build will be called again
-  final List <String> products;
+  final List<String> products;
 
   Products([this.products = const []]) {
     print('[Products Widget] Constructor');
   }
 
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+        child: Column(
+      children: <Widget>[Image.asset('assets/food.jpg'), Text(products[index])],
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return ListView(
-      children: products.map((element) => Card(
-        child: Column(
-          children: <Widget>[
-            Image.asset('assets/food.jpg'),
-            Text(element)
-          ],
-        ),
-      ))
-          .toList(),
+    return ListView.builder(
+      itemBuilder: _buildProductItem,
+      itemCount: products.length,
     );
+//      (
+//      children: products.map((element) => Card(
+//        child: Column(
+//          children: <Widget>[
+//            Image.asset('assets/food.jpg'),
+//            Text(element)
+//          ],
+//        ),
+//      ))
+//          .toList(),
+//    );
   }
 }
