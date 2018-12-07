cc71826149602c11f04aab87f85a0ca07c5d52b2
Chris Weicht <chris.weicht@gmail.com>
2018-12-07 07:30:28 -0500

    wrapping up navigation.

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -5,6 +5,17 @@ class ProductCreatePage extends StatelessWidget {
   // - not a full Scaffold widget
   @override
   Widget build(BuildContext context) {
-    return Text("Product create page coming soon");
+    return Center(
+        child: RaisedButton(
+            child: Text('Save'),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Center(
+                      child: Text('Modal from bottom'),
+                    );
+                  });
+            }));
   }
 }
